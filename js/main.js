5f1522df19cbd3a853f0ed0a52de9969ac93d73f
Fabio Buchanan <fabiobuchanan@gmail.com>
2023-04-05 20:29:38 -0300

    Commit preentrega JavaScript

--- js/main.js
+++ js/main.js
@@ -1,61 +1,235 @@
-let producto = prompt("¿Qué producto estás buscando: Suscripcion o Clase?");
 const PRODUCTO_UNO = "SUSCRIPCION";
 const PRODUCTO_DOS = "CLASE";
-let productoClases = '';
-let productoSuscripciones = '';
-let mensual = 20; 
+let productoClases = "";
+let productoSuscripciones = "";
+let mensual = 20;
 let semestral = 18;
-let anual = 16; 
+let anual = 16;
 let paqueteTres = 10;
 let paqueteCinco = 16;
 let paqueteSiete = 24;
 let paqueteNueve = 30;
+let validar;
 
-if(producto.toUpperCase() == PRODUCTO_UNO || producto.toLowerCase() == PRODUCTO_UNO){
-    productoSuscripciones = prompt("Elige entre las siguientes opciones: Mensual, Semestral, Anual. O bien el porcentaje deseado para cada suscripción, respectivamente: 20, 18, 16.");
-    console.log("Elige entre las siguientes opciones: Mensual, Semestral, Anual. O bien el porcentaje deseado para cada suscripción, respectivamente: 20, 18, 16.");
-    switch(productoSuscripciones){
-        case "mensual":
-        case "20":
-            alert("Has seleccionado la suscripción Mensual. Precio: " + mensual + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            console.log("Has seleccionado la suscripción Mensual. Precio: " + mensual + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            break;
-        case "semestral":
-        case "18":
-            alert("Has seleccionado la suscripción Semestral. Precio: " + semestral + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            console.log("Has seleccionado la suscripción Semestral. Precio: " + semestral + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            break;
-        case "anual":
-        case "16":
-            alert("Has seleccionado la suscripción Anual. Precio: " + anual + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            console.log("Has seleccionado la suscripción Anual. Precio: " + anual + " " + "dólares." + " " + "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos.");
-            break;    
-        default:
-            prompt("Suscripción ingresada inexistente. Por favor, intente nuevamente.");
-    }
-} else if(producto.toUpperCase() == PRODUCTO_DOS || producto.toLowerCase() == PRODUCTO_DOS){
-    productoClases = Number(prompt("Elige entre las siguientes opciones: 3, 5, 7, 9. O bien el porcentaje deseado para cada paquete de clases, respectivamente: 17, 20, 15, 17."));
-    console.log("Elige entre las siguientes opciones: 3, 5, 7, 9. O bien el porcentaje deseado para cada paquete de clases, respectivamente: 17, 20, 15, 17.");
-    switch(productoClases){
-        case 3:
-            alert("Has seleccionado el paquete de " + productoClases + " " + "por el precio de " + paqueteTres + " " + "dólares." + " ");
-            console.log("Has seleccionado el paquete de " + productoClases+ "por el precio de " + paqueteTres + " " + "dólares." + " ");
-            break;
-        case 5:
-            alert("Has seleccionado el paquete de " + productoClases +  " " + "por el precio de " + paqueteCinco + " " + "dólares." + " ");
-            console.log("Has seleccionado el paquete de " + productoClases+ "por el precio de " + paqueteCinco + " " + "dólares." + " ");
-            break;
-        case 7:
-            alert("Has seleccionado el paquete de " + productoClases + " " + "por el precio de " + paqueteSiete + " " + "dólares." + " ");
-            console.log("Has seleccionado el paquete de " + productoClases + "por el precio de " + paqueteSiete + " " + "dólares." + " ");
-            break;
-        case 9:
-            alert("Has seleccionado el paquete de " + productoClases +  " " + "por el precio de " + paqueteNueve + " " + "dólares." + " ");
-            console.log("Has seleccionado el paquete de " + productoClases + "por el precio de " + paqueteNueve + " " + "dólares." + " ");
-            break;    
-        default:
-            productoSuscripciones = "Clase ingresada inexistente. Por favor, intente nuevamente."
-    }
-  } else if(producto !== PRODUCTO_UNO && producto !== PRODUCTO_DOS){
-        prompt("Ha ingresado información inválida. Por favor intente nuevamente.");
+// DECLARO FUNCIONES
+
+const esValidoProducto = (prod) => {
+  let valido = false;
+  if (prod === PRODUCTO_UNO || prod === PRODUCTO_DOS) valido = true;
+  return valido;
+};
+
+const esValidoSuscripcion = (susc) => {
+  let valido = false;
+  switch (susc) {
+    case "MENSUAL":
+    case "20":
+    case "SEMESTRAL":
+    case "18":
+    case "ANUAL":
+    case "16":
+      valido = true;
+      break;
+  }
+  return valido;
+};
+
+const esValidoClase = (clase) => {
+  let valido = false;
+  switch (clase) {
+    case 3:
+    case 5:
+    case 7:
+    case 9:
+    case 17:
+    case 20:
+    case 15:
+    case 18:
+      valido = true;
+      break;
+  }
+  return valido;
+};
+
+// BEGIN
+
+do {
+  producto = prompt("¿Qué producto quiere seleccionar: Suscripcion o Clase?");
+  producto = producto.toUpperCase();
+  //validar = esValido(producto);
+} while (!esValidoProducto(producto));
+
+if (producto === PRODUCTO_UNO) {
+  do {
+    productoSuscripciones = prompt(
+      "Elige entre las siguientes opciones: Mensual, Semestral, Anual. O bien el porcentaje deseado para cada suscripción, respectivamente: 20, 18, 16."
+    );
+    productoSuscripciones = productoSuscripciones.toUpperCase();
+  } while (!esValidoSuscripcion(productoSuscripciones));
+
+  console.log(
+    "Elige entre las siguientes opciones: Mensual, Semestral, Anual. O bien el porcentaje deseado para cada suscripción, respectivamente: 20, 18, 16."
+  );
+  switch (productoSuscripciones) {
+    case "MENSUAL":
+    case "20":
+      alert(
+        "Has seleccionado la suscripción Mensual. Precio: " +
+          mensual +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      console.log(
+        "Has seleccionado la suscripción Mensual. Precio: " +
+          mensual +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      break;
+    case "SEMESTRAL":
+    case "18":
+      alert(
+        "Has seleccionado la suscripción Semestral. Precio: " +
+          semestral +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      console.log(
+        "Has seleccionado la suscripción Semestral. Precio: " +
+          semestral +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      break;
+    case "ANUAL":
+    case "16":
+      alert(
+        "Has seleccionado la suscripción Anual. Precio: " +
+          anual +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      console.log(
+        "Has seleccionado la suscripción Anual. Precio: " +
+          anual +
+          " " +
+          "dólares." +
+          " " +
+          "Tendrás acceso a toda la plataforma y su contenido, tanto clases como cursos."
+      );
+      break;
+  }
+}
+
+if (producto === PRODUCTO_DOS) {
+  do {
+    productoClases = Number(
+      prompt(
+        "Elige entre las siguientes opciones: 3, 5, 7, 9. O bien el porcentaje deseado para cada paquete de clases, respectivamente: 17, 20, 15, 17."
+      )
+    );
+  } while (!esValidoClase(productoClases));
+
+  console.log(
+    "Elige entre las siguientes opciones: 3, 5, 7, 9. O bien el porcentaje deseado para cada paquete de clases, respectivamente: 17, 20, 15, 17."
+  );
+  switch (productoClases) {
+    case 3:
+      alert(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          " " +
+          "por el precio de " +
+          paqueteTres +
+          " " +
+          "dólares." +
+          " "
+      );
+      console.log(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          "por el precio de " +
+          paqueteTres +
+          " " +
+          "dólares." +
+          " "
+      );
+      break;
+    case 5:
+      alert(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          " " +
+          "por el precio de " +
+          paqueteCinco +
+          " " +
+          "dólares." +
+          " "
+      );
+      console.log(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          "por el precio de " +
+          paqueteCinco +
+          " " +
+          "dólares." +
+          " "
+      );
+      break;
+    case 7:
+      alert(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          " " +
+          "por el precio de " +
+          paqueteSiete +
+          " " +
+          "dólares." +
+          " "
+      );
+      console.log(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          "por el precio de " +
+          paqueteSiete +
+          " " +
+          "dólares." +
+          " "
+      );
+      break;
+    case 9:
+      alert(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          " " +
+          "por el precio de " +
+          paqueteNueve +
+          " " +
+          "dólares." +
+          " "
+      );
+      console.log(
+        "Has seleccionado el paquete de " +
+          productoClases +
+          "por el precio de " +
+          paqueteNueve +
+          " " +
+          "dólares." +
+          " "
+      );
+      break;
+    default:
+      productoSuscripciones =
+        "Clase ingresada inexistente. Por favor, intente nuevamente.";
+  }
 }
